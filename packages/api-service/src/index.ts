5115903889a9535e382250a0974ad17e9f7b0dc3
fede1984br <fede1984br@gmail.com>
2025-09-09 08:06:31 -0500

    Update index.ts

--- packages/api-service/src/index.ts
+++ packages/api-service/src/index.ts
@@ -3,5 +3,7 @@ import { usersApp, integrationsApp } from "./app";
 
 // Expose the Express apps as a Cloud Functions
 
+const serviceAccount = "myphantomdev@appspot.gserviceaccount.com";
+
 export const users = onRequest(usersApp);
 export const integrations = onRequest(integrationsApp);
