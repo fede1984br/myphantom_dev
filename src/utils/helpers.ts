7a80fd1536cf77b694f8a2125dfee18223ff3988
Ricardo <ricardo@Ricardos-MacBook-Air.local>
2025-09-17 09:50:54 -0500

    feat: Implement workspace and resolve merge conflict

---
+++ src/utils/helpers.ts
@@ -0,0 +1,4 @@
+// src/utils/helpers.ts
+export const myUtilityFunction = () => {
+  console.log("This is a utility function!");
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./Layout.js",
-    "./Pages/**/*.{js,ts,jsx,tsx}",
-    "./Components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
